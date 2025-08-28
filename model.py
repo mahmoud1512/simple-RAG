8c2db96ae4458f56afbfa232208045e263706fcb
mahmoud1512 <120992403+mahmoud1512@users.noreply.github.com>
2025-08-28 07:20:37 +0300

    implementing base model code

---
+++ model.py
@@ -0,0 +1,21 @@
+
+class RAG():
+    def __init__(self):
+        self.text=None
+
+
+    def embed_query(self,query:str):
+        pass
+
+
+    def embed_chunk(self,text_chunk:str):
+        pass
+
+    def store_in_vector_DB(self):
+        pass
+
+    def get_similar_chunks_to_query(self):
+        pass
+
+    def formulate_answer(self):
+        pass
